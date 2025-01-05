1dc4789f496a5c3c3b1b5d3d7ed42d40e70567a4
duck420i69 <136151894+ddduc21@users.noreply.github.com>
2025-01-05 18:51:11 +0700

    commit

--- src/components/features/auth/RegisterForm.tsx
+++ src/components/features/auth/RegisterForm.tsx
@@ -23,11 +23,13 @@ export const RegisterForm = () => {
         confirm_password: string
     }
 
-<<<<<<< HEAD
-    const {register, handleSubmit, watch, reset, formState: { errors, isSubmitSuccessful }} = useForm<Inputs>()
-=======
-    const { register, handleSubmit, watch, reset, formState: { errors, isSubmitSuccessful } } = useForm<Inputs>()
->>>>>>> main
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors, isSubmitSuccessful },
+    } = useForm<Inputs>()
     const onSubmit: SubmitHandler<Inputs> = async (data) => {
         try {
             const userData: UserData = {
@@ -73,45 +75,27 @@ export const RegisterForm = () => {
                 <div className="text-blue-500 font-bold text-3xl mb-3 text-center">Sign-up</div>
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
                     <div className="mb-1">
-                        <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Username</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="username"
-                            type="text"
-                            {...register("username", { required: "This field is required" })} />
-                        {errors.username &&
-                            <div className='text-xs text-left mt-1 text-red-700'>
-                                {errors.username.message}
-                            </div>}
-<<<<<<< HEAD
-=======
+                        <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Email</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" {...register("email", {
+                            required: "This field is required",
+                            pattern: {
+                                value: /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/,
+                                message: "Invalid email"
+                            }
+                        })} />
+                        {errors.email && <div className='text-xs text-left mt-1 text-red-700'>{errors.email.message}</div>}
                     </div>
 
                     <div className="mb-1">
-                        <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Email</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="email"
-                            type="text"
-                            {...register("email", {
-                                required: "This field is required",
-                                pattern: {
-                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                    message: "Invalid email address"
-                                }
-                            })} />
-                        {errors.email &&
-                            <div className='text-xs text-left mt-1 text-red-700'>
-                                {errors.email.message}
-                            </div>}
->>>>>>> main
+                        <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Username</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" {...register("username", { required: "This field is required" })} />
+                        {errors.username && <div className='text-xs text-left mt-1 text-red-700'>{errors.username.message}</div>}
                     </div>
 
                     <div className="mb-1">
                         <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Password</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password"
-                            type="password"
-<<<<<<< HEAD
-                            {...register("password", { required: "This field is required",
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" {...register("password", {
+                            required: "This field is required",
                             maxLength: {
                                 value: 20,
                                 message: "Password must be at most 20 characters long"
@@ -122,33 +106,13 @@ export const RegisterForm = () => {
                                 }
                             }
                         })} />
-=======
-                            {...register("password", {
-                                required: "This field is required",
-                                maxLength: {
-                                    value: 20,
-                                    message: "Password must be at most 20 characters long"
-                                },
-                                validate: (val: string) => {
-                                    if (watch('confirm_password') !== val) {
-                                        return "Your password does no match";
-                                    }
-                                }
-                            })} />
->>>>>>> main
-                        {errors.password &&
-                            <div className='text-xs text-left mt-1 text-red-700'>
-                                {errors.password.message}
-                            </div>}
+                        {errors.password && <div className='text-xs text-left mt-1 text-red-700'>{errors.password.message}</div>}
                     </div>
 
                     <div className="mb-1">
-                        <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Confirm-password</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="confirm-password"
-                            type="password"
-<<<<<<< HEAD
-                            {...register("confirm_password", { required: "This field is required",
+                        <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Confirm-Password</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="confirm-password" type="password" {...register("confirm_password", {
+                            required: "This field is required",
                             maxLength: {
                                 value: 20,
                                 message: "Password must be at most 20 characters long"
@@ -159,24 +123,7 @@ export const RegisterForm = () => {
                                 }
                             }
                         })} />
-=======
-                            {...register("confirm_password", {
-                                required: "This field is required",
-                                maxLength: {
-                                    value: 20,
-                                    message: "Password must be at most 20 characters long"
-                                },
-                                validate: (val: string) => {
-                                    if (watch('password') !== val) {
-                                        return "Your password does no match";
-                                    }
-                                }
-                            })} />
->>>>>>> main
-                        {errors.confirm_password &&
-                            <div className='text-xs text-left mt-1 text-red-700'>
-                                {errors.confirm_password.message}
-                            </div>}
+                        {errors.confirm_password && <div className='text-xs text-left mt-1 text-red-700'>{errors.confirm_password.message}</div>}
                     </div>
 
                     <div className="flex flex-col justify-center mt-3 mx-auto">
